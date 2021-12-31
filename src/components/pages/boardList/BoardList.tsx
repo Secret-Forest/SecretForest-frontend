15a34bcc79fdcb6c0e813171e18f04f8912c730a
baejoonsoo <baejoonsoo21@gmail.com>
2021-12-31 19:18:47 +0900

    Feat: board list

--- src/components/pages/boardList/BoardList.tsx
+++ src/components/pages/boardList/BoardList.tsx
@@ -1,9 +1,60 @@
 import * as S from "./styled";
 import searchBtn from "../../../assets/img/searchBtn.svg";
+
+const BoardData = [
+  {
+    id: 1,
+    title: "내가하는 이야길 잘 들어봐",
+    writer: "Java 혐오자",
+  },
+  {
+    id: 2,
+    title: "내가하는 이야길 잘 들어봐",
+    writer:
+      "Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자Java 혐오자",
+  },
+  {
+    id: 3,
+    title: "내가하는 이야길 잘 들어봐",
+    writer: "Java 혐오자",
+  },
+  {
+    id: 4,
+    title: "내가하는 이야길 잘 들어봐",
+    writer: "Java 혐오자",
+  },
+  {
+    id: 5,
+    title: "내가하는 이야길 잘 들어봐",
+    writer: "Java 혐오자",
+  },
+  {
+    id: 6,
+    title:
+      "어떤 것은 검은색 어떤 것은 하얀색 색안경을 끼고 보면 어떡해. 넌 착한 사람이고 걘 나쁜 사람이고, 재미없는 너의 세상은 흑백",
+    writer: "스텔라장",
+  },
+  {
+    id: 7,
+    title:
+      "늦게 다니지좀 마. 술은 멀리좀 해봐. 열살짜리 애처럼 말을 안듣니. 정말 웃음만 나와. 누가 누굴보고 아이라 하는지 정말 웃음만 나와.",
+    writer: "잔소리",
+  },
+];
+
 const BoardList = () => {
   return (
     <S.ListPage>
-      <S.BoardList></S.BoardList>
+      <S.BoardList>
+        {BoardData.map((data) => (
+          <S.Board key={data.id}>
+            {/* <div> */}
+            <h1>{data.title}</h1>
+            <h2>{data.writer}</h2>
+            {/* </div> */}
+          </S.Board>
+        ))}
+      </S.BoardList>
       <S.Search>
         <S.SearchBar>
           <S.SearchInput placeholder="제목 혹은 닉네임을 입력해주세요." />
--- src/components/pages/boardList/styled.ts
+++ src/components/pages/boardList/styled.ts
@@ -1,19 +1,11 @@
 import styled from "styled-components";
 
-// overflow: scroll;
 export const ListPage = styled.main`
   width: 90vw;
-  height: 50rem;
-  /* background-color: red; */
-  margin: 2.5rem auto 0 auto;
+  height: 48rem;
+  margin: 2.5rem auto 3.5rem auto;
   display: flex;
   justify-content: space-between;
-`;
-
-export const BoardList = styled.section`
-  height: 100%;
-  width: 70%;
-  background-color: blue;
 `;
 
 export const Search = styled.aside`
@@ -21,7 +13,6 @@ export const Search = styled.aside`
   width: 25%;
   display: grid;
   justify-items: center;
-  /* background-color: yellow; */
 `;
 
 export const SearchInput = styled.input`
@@ -53,7 +44,78 @@ export const SearchBar = styled.div`
 
 export const SuggestedSearchTerm = styled.div`
   width: 100%;
-  height: 46rem;
+  height: 44rem;
   margin-top: 1rem;
   border: 2px solid #a0a0a0;
+`;
+
+export const BoardList = styled.section`
+  max-height: 100%;
+  width: 70%;
+  height: fit-content;
+  border-top: #2f4c3c 5px solid;
+  border-bottom: #2f4c3c 3px solid;
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  scrollbar-color: #415f4e #a0a0a0;
+  scrollbar-width: thin;
+
+  &::-webkit-scrollbar {
+    width: 7px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #415f4e;
+    border-radius: 3.5px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: grey;
+    border-radius: 3.5px;
+  }
+`;
+
+export const Board = styled.div`
+  height: 8rem;
+  min-width: 99%;
+  border: #2f4c3c 0.5px solid;
+  background-color: #bdd8c9;
+  display: grid;
+  /* justify-items: center; */
+  align-items: center;
+  cursor: pointer;
+  &:active {
+    transform: scale(1.01);
+  }
+
+  h1 {
+    color: #324d3b;
+    font-size: 30px;
+    font-weight: 500;
+
+    width: 90%;
+    display: block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    margin-bottom: 10px;
+
+    align-self: flex-end;
+    padding-left: 20px;
+  }
+
+  h2 {
+    color: #72a081;
+    font-size: 20px;
+    font-weight: 500;
+
+    width: 50%;
+    display: block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    margin-bottom: 10px;
+    padding-left: 40px;
+
+    align-self: flex-start;
+  }
 `;
