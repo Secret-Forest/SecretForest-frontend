7c50304b7c35dd5dbef8ecdba41e4a2342844673
baejoonsoo <baejoonsoo21@gmail.com>
2022-01-05 16:22:52 +0900

    Feat: admin header

---
+++ src/components/pages/admin/adminMain/AdminMain.tsx
@@ -0,0 +1,12 @@
+import * as S from "./style";
+const AdminMain = () => {
+  return (
+    <S.MainPage>
+      <S.ListSection></S.ListSection>
+      <S.ListSection></S.ListSection>
+      <S.ListSection></S.ListSection>
+    </S.MainPage>
+  );
+};
+
+export default AdminMain;
---
+++ src/components/pages/admin/adminMain/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+//384B3C
+export const MainPage = styled.main`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const ListSection = styled.section`
+  background-color: red;
+  height: 40rem;
+  width: 25%;
+  margin-top: 150px;
+`;
