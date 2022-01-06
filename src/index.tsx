54dda17aae2657a4129ab7b72cb9f964257a20f4
baejoonsoo <baejoonsoo21@gmail.com>
2022-01-06 18:00:33 +0900

    Feat: Blocking IE
    IE로 접속할 시 페이지를 띄우지 않는다

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const agent: string = navigator.userAgent.toLowerCase();
+
+if (
+  (navigator.appName === "Netscape" &&
+    navigator.userAgent.search("Trident") !== -1) ||
+  agent.indexOf("msie") !== -1
+) {
+  alert("Internet Explorer는 호환되지 않는 브라우저 입니다.");
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 reportWebVitals();
