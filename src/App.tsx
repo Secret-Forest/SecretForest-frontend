d2f80d7f166b8a5467c2db46e3b9fbadb6df3345
baejoonsoo <baejoonsoo21@gmail.com>
2022-01-07 00:18:32 +0900

    Feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-// import Header from "./components/common/header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/common/header/Header";
 import AdminMain from "./components/pages/admin/adminMain/AdminMain";
 import AdminHeader from "./components/pages/admin/header/AdminHeaer";
 import Login from "./components/pages/admin/login/Login";
 import BoardList from "./components/pages/boardList/BoardList";
-// import ShowPost from "./components/pages/showPost/ShowPost";
-// import WritePost from "./components/pages/writePost/writePost";
+import ShowPost from "./components/pages/showPost/ShowPost";
+import WritePost from "./components/pages/writePost/writePost";
 
 function App() {
   return (
-    <div>
-      <AdminHeader />
-      <Login />
-      {/* <AdminMain />
-      <BoardList /> */}
-      {/* <WritePost /> */}
-      {/* <ShowPost /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AdminMain />} path="/admin" />
+        <Route element={<Login />} path="/admin/login" />
+        <Route element={<BoardList />} path="/list" />
+        <Route element={<WritePost />} path="/write" />
+        <Route element={<ShowPost />} path="/show" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
