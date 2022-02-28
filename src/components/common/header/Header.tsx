151eb798e1d0930b11088c2674c5c1e7545f5189
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-01 01:46:16 +0900

    Feat: write post page link button

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,7 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import * as S from "../../../styles/header";
 import logo from "../../../assets/image/headerLogo.png";
-
+import edit from "../../../assets/image/edit.svg";
 const Header = () => {
   return (
     <>
@@ -9,6 +9,11 @@ const Header = () => {
         <Link to="/" style={{ textDecoration: "none" }}>
           <S.HeaderLogo src={logo} alt="header logo" />
         </Link>
+        <S.Write>
+          <Link to="/write" style={{ textDecoration: "none" }}>
+            <img src={edit} alt="write new post" />
+          </Link>
+        </S.Write>
       </S.Header>
       <Outlet />
     </>
--- src/styles/header.ts
+++ src/styles/header.ts
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
-export const HeaderLogo = styled.img`
-  height: 60px;
-`;
 export const Header = styled.header`
   background-color: #2c523d;
   width: 100%;
   padding: 0.5vw 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderLogo = styled.img`
+  height: 60px;
+`;
+
+export const Write = styled.button`
+  border: none;
+  margin-right: 20px;
+
+  background-color: transparent;
+
+  &:active {
+    transform: scale(0.95);
+  }
+
+  img {
+    width: 25px;
+  }
 `;
